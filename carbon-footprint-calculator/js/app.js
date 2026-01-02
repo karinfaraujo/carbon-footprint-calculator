bbfe4fd84800551b7e3208757144f6b126787499
Karin F. Araujo <125605415+karinfaraujo@users.noreply.github.com>
2026-01-02 17:30:19 -0300

    Delete carbon-footprint-calculator directory

--- carbon-footprint-calculator/js/app.js
+++
@@ -1,8 +0,0 @@
-import { populateCities, handleFormSubmit } from "./ui.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  populateCities();
-
-  const form = document.getElementById("carbon-form");
-  form.addEventListener("submit", handleFormSubmit);
-});
